12059be7bdf13a6538f6dab1bf4ee85fddcd0a72
afiq-aswadi <aeffiezalasw@student.unimelb.edu.au>
2024-12-15 12:13:25 +0800

    change to tooltip

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
 from .ui_editor import init_editor
 from .ui_menu import init_menu
-from aqt.utils import showInfo
 from . import utils
+from aqt.utils import tooltip
 
 config = utils.get_config()
 if not config.get('api_key'):
-    showInfo("Please set your API key in Tools > Anki-Copilot > Settings")
+    tooltip("Please set your API key in Tools > Anki-Copilot > Settings")
 
 init_editor()
 init_menu()
